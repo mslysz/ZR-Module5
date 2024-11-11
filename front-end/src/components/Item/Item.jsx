572dbd5533bc4eb2efa50e39876d9c84b44119cc
mslysz <macslysz@gmail.com>
2024-11-11 04:23:07 +0000

    added event handler logic, adding, deleting and displaying errors when the API is not responding or another error has occurred

--- front-end/src/components/Item/Item.jsx
+++ front-end/src/components/Item/Item.jsx
@@ -1,34 +1,32 @@
-import { useState } from "react";
-import styles from "./Item.module.css";
-export function Item({ word, translation, id }) {
-    const [isTranslationShown, setIsTranslationShown] = useState(false);
+import { useState } from 'react';
+import styles from './Item.module.css';
+export function Item({ word, translation, id, onDeleteItem }) {
+  const [isTranslationShown, setIsTranslationShown] = useState(false);
 
-    function handleDeleteClick() {
-        alert(`Delete element with ID: ${id}`);
-    }
+  function handleDeleteClick() {
+    onDeleteItem(id);
+  }
 
-    return (
-        <li className={styles.item}>
-            <span className={styles.word}>
-                (PL) <strong>{word}</strong>
-            </span>
-            <span className={styles.word}>
-                (ANG)
-                <strong> {isTranslationShown ? translation : "******"}</strong>
-            </span>
-            <div className={styles.buttons}>
-                <button
-                    onClick={() =>
-                        setIsTranslationShown((prevValue) => !prevValue)
-                    }
-                    className={styles.button}
-                >
-                    👁️
-                </button>
-                <button onClick={handleDeleteClick} className={styles.button}>
-                    ✔️
-                </button>
-            </div>
-        </li>
-    );
+  return (
+    <li className={styles.item}>
+      <span className={styles.word}>
+        (PL) <strong>{word}</strong>
+      </span>
+      <span className={styles.word}>
+        (ANG)
+        <strong> {isTranslationShown ? translation : '******'}</strong>
+      </span>
+      <div className={styles.buttons}>
+        <button
+          onClick={() => setIsTranslationShown((prevValue) => !prevValue)}
+          className={styles.button}
+        >
+          👁️
+        </button>
+        <button onClick={handleDeleteClick} className={styles.button}>
+          ✔️
+        </button>
+      </div>
+    </li>
+  );
 }
--- front-end/src/components/List/List.jsx
+++ front-end/src/components/List/List.jsx
@@ -1,11 +1,17 @@
-import { Item } from "../Item/Item";
-import styles from "./List.module.css";
-export function List({ data }) {
-    return (
-        <ul className={styles.list}>
-            {data.map(({ id, word, translation }) => (
-                <Item key={id} id={id} word={word} translation={translation} />
-            ))}
-        </ul>
-    );
+import { Item } from '../Item/Item';
+import styles from './List.module.css';
+export function List({ data, onDeleteItem }) {
+  return (
+    <ul className={styles.list}>
+      {data.map(({ id, word, translation }) => (
+        <Item
+          key={id}
+          id={id}
+          word={word}
+          translation={translation}
+          onDeleteItem={onDeleteItem}
+        />
+      ))}
+    </ul>
+  );
 }
