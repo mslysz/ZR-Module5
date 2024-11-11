ab5f9e9d64d3dc4b7ab908752400794af6471c30
mslysz <macslysz@gmail.com>
2024-11-11 01:59:52 +0000

    initial commit

---
+++ front-end/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import styles from './App.module.css';
+import { Panel } from './components/Panel/Panel';
+import { Button } from './components/Button/Button';
+
+function App() {
+  const [isPanelShown, setIsPanelShown] = useState(true);
+
+  return (
+    <main className={styles.main}>
+      <Button
+        onClick={() => {
+          setIsPanelShown((prevIsPanelShown) => !prevIsPanelShown);
+        }}
+      >
+        {isPanelShown ? 'Schowaj panel' : 'Pokaż panel'}
+      </Button>
+      {isPanelShown && <Panel />}
+    </main>
+  );
+}
+
+export default App;
